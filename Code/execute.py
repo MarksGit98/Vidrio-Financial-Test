04ea797362cad31e1121f95d50216623ce60fad1
MarksGit98 <markbekker1998@gmail.com>
2021-02-11 16:38:01 -0500

    finished up to and including part 7

--- Code/execute.py
+++ Code/execute.py
@@ -10,12 +10,12 @@ def main():
         'Portfolio Account NAV.xlsx'
     ]
     read_and_write = ReadandWrite()
-#     read_and_write.excel_to_db(input_files)
+    # read_and_write.excel_to_db(input_files)
     # read_and_write.run_query("SELECT * FROM 'index data'")
     # print(read_and_write.extract_timestamp_from_file(input_files[0]))
-    read_and_write.generate_portfolio_valuation_file1(input_files, "DAV Proforma Acc Analy.xlsx")
-    read_and_write.generate_portfolio_valuation_file2(input_files[0], "Portfolio Account NAV.xlsx")
-    read_and_write.excel_to_db(generated_files)
+    # read_and_write.generate_portfolio_valuation_file1(input_files, "DAV Proforma Acc Analy.xlsx")
+    # read_and_write.generate_portfolio_valuation_file2(input_files[0], "Portfolio Account NAV.xlsx")
+    # read_and_write.excel_to_db(generated_files)
     read_and_write.convert_xlsx_to_xml(generated_files)
 
 if __name__ == "__main__":
